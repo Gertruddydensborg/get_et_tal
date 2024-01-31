61dc1f7307146e690e881b4ae5a547669c2723a9
gertruddydensborg <gertruddydensborg1@gmail.com>
2024-01-31 13:40:43 +0100

    gæt et tal

---
+++ script.js
@@ -0,0 +1,7 @@
+window.addEventListener("load", sidenVises);
+
+function tjekGet() {
+  var getTal = document.getElementById("tal").value;
+
+  var rigtigtTal = Math.floor(Math.random() * 11) + 1;
+}
